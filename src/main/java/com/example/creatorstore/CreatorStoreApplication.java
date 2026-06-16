3d390d79696e9a28d62afdbcca138b90ad42d2b2
KasperLinux <kushaldas@gmail.com>
2026-06-16 11:11:36 -0700

    chore: add dotenv java for the application

--- src/main/java/com/example/creatorstore/CreatorStoreApplication.java
+++ src/main/java/com/example/creatorstore/CreatorStoreApplication.java
@@ -1,12 +1,18 @@
 package com.example.creatorstore;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CreatorStoreApplication {
 
-    public static void main(String[] args) {
+     static void main(String[] args) {
+        //Configuration of dotenv-java
+        Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+        dotenv.entries().forEach((entry) -> System.setProperty(
+                entry.getKey(), entry.getValue()
+        ));
         SpringApplication.run(CreatorStoreApplication.class, args);
     }
 

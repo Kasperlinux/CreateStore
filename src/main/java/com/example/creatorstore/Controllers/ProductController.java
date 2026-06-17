f357fb94a44c27e24dadfd9c8fcfa3de4d38f0c5
KasperLinux <kushaldas@gmail.com>
2026-06-17 10:15:37 -0700

    chore: added repositories and working on controller

---
+++ src/main/java/com/example/creatorstore/Controllers/ProductController.java
@@ -0,0 +1,35 @@
+package com.example.creatorstore.Controllers;
+
+import com.example.creatorstore.entities.Product;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/products")
+public class ProductController {
+
+    @PostMapping
+    public Product createProduct(@Valid @RequestBody Product product){
+        return null;
+    }
+
+    @PutMapping("/{id}")
+    public Product updateProduct(@PathVariable Long id,@Valid @RequestBody Product product){
+        return null;
+    }
+
+
+    public List<Product> getProducts(){
+        return null;
+    }
+
+    public Product getProductById(Long id){
+        return null;
+    }
+
+    public void deleteProduct(Long id){}
+}
+
+

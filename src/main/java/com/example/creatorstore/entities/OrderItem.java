5b16eeef273833115a9268a251b4f5620a0f5872
KasperLinux <kushaldas@gmail.com>
2026-06-18 09:28:30 -0700

    chore: added repositories and working on controller

--- src/main/java/com/example/creatorstore/entities/OrderItem.java
+++ src/main/java/com/example/creatorstore/entities/OrderItem.java
@@ -1,7 +1,6 @@
 package com.example.creatorstore.entities;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
 import lombok.*;
 
